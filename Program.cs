cc48478b41a9d37cd9fe97dfae20925153fb59ed
Apocrifon <Astakhov.stepan2003@gmail.com>
2022-09-08 20:40:05 +0500

    1ver

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GraphMop
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+
+    class Matrix
+    { 
+
+    }
+
+}
