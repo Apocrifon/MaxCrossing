8d19f1e4ac803cc8a624d544ff9df52c76039bc7
Apocrifon <Astakhov.stepan2003@gmail.com>
2022-09-14 00:27:03 +0500

    add some graphs

--- Graph.cs
+++ Graph.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GraphMop
 {
@@ -12,7 +7,8 @@ namespace GraphMop
         public readonly int  Left;
         public readonly int Right;
         public readonly int Size;
-        public int[,] WeightedGraph;  // нагруженный граф
+        public int[,] ConnectionGraph; //матрица связности
+        public int[,] WeightedGraph; // нагруженный граф
         public int[,] IncrementGraph; // граф приращения
         public int[,] Weight;
         public Graph(int left, int right)
@@ -20,26 +16,68 @@ namespace GraphMop
             Size = left+right+2;
             Left=left;
             Right=right;
-            WeightedGraph = new int[Size, Size];
+            ConnectionGraph = new int[Size, Size];
+            WeightedGraph= new int[Size, Size];
+            IncrementGraph = new int[Size, Size];
         }
 
-        public void SetWeightGraph(int[,] matrix)
+        public static void PringGraph(int[,] graph)
+        {
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                for (int j = 0; j < graph.GetLength(0); j++)
+                {
+                    Console.Write(graph[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void SetConnectionGraph(int[,] matrix)
         {
             for (int i = 1; i < Left+1 ; i++)
             {
-                WeightedGraph[0,i] = 1;
-                WeightedGraph[i, 0] = 1;
+                ConnectionGraph[0,i] = 1;
+                ConnectionGraph[i, 0] = 1;
             }
             for (int i = Size-2; i > Left ; i--)
             {
-                WeightedGraph[Size-1, i] = 1;
-                WeightedGraph[i, Size - 1] = 1;
+                ConnectionGraph[Size-1, i] = 1;
+                ConnectionGraph[i, Size - 1] = 1;
             }
             for (int i = 0; i < Left+Right; i++)
             {
                 for (int j = 0; j < Left + Right; j++)
                 {
-                    WeightedGraph[i+1,j+1]=matrix[i,j];
+                    ConnectionGraph[i+1,j+1]=matrix[i,j];
+                }
+            }
+        }
+
+        public void SetWeightGraph()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (ConnectionGraph[i,j]== 1)
+                        WeightedGraph[i, j] = 0;
+                    else
+                        IncrementGraph[i, j] = -1;
+                }
+            }
+        }
+
+        public void SetIncrementGraph()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (WeightedGraph[i, j] != 1)
+                        IncrementGraph[i, j] = 0;
+                    else
+                        IncrementGraph[i, j] = -1;
                 }
             }
         }
--- Program.cs
+++ Program.cs
@@ -6,25 +6,20 @@ namespace GraphMop
     {
         static void Main()
         {
-            //Console.Write("Укажите кол-во вершин первой доли -> ");
-            //var leftPart = int.Parse(Console.ReadLine());
-            //Console.Write("Укажите кол-во вершин второй доли -> ");
-            //var rightPart = int.Parse(Console.ReadLine());
-            //var matrix = new Matrix(leftPart + rightPart);
-            //Console.WriteLine("Укажите ребра соединяющие вершины каждой доли, чтобы выйди из редактирование введите \"-1\"");
-            //matrix.SetMatrixEdges();
-            //matrix.PrintAdjacencyMatrix();
-            //var weightedGraph = new Graph(leftPart,rightPart);
-            var graph = new Graph(4, 2);
-            graph.SetWeightGraph(new int[6,6]);
-            for (int i = 0; i < graph.Size; i++)
-            {
-                for (int j = 0; j < graph.Size; j++)
-                {
-                    Console.Write(graph.WeightedGraph[i, j]); 
-                }
-                Console.WriteLine();
-            }
+            Console.Write("Укажите кол-во вершин первой доли -> ");
+            var leftPart = int.Parse(Console.ReadLine());
+            Console.Write("Укажите кол-во вершин второй доли -> ");
+            var rightPart = int.Parse(Console.ReadLine());
+            var matrix = new Matrix(leftPart + rightPart);
+            Console.WriteLine("Укажите ребра соединяющие вершины каждой доли, чтобы выйди из редактирование введите \"-1\"");
+            matrix.SetMatrixEdges();
+            matrix.PrintAdjacencyMatrix();
+            var graph = new Graph(leftPart, rightPart);
+            graph.SetConnectionGraph(matrix.AdjacencyMatrix);
+            graph.SetWeightGraph();
+            graph.SetIncrementGraph();
+            Graph.PringGraph(graph.IncrementGraph);
+
         }
     }
 }
